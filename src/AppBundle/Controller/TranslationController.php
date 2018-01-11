9f48f4c6b1418a73e08144359882f363aeab29ca
philipdangpro <philip.dang.pro@gmail.com>
2018-01-11 16:54:29 +0100

    i18n et +

--- src/AppBundle/Controller/TranslationController.php
+++ src/AppBundle/Controller/TranslationController.php
@@ -5,6 +5,9 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Translation\TranslatorInterface;
 
 /**
  * @Route("/{_locale}")
@@ -15,9 +18,25 @@ class TranslationController extends Controller
     /**
      * @Route("/translation", name="translation.index")
      */
-    public function indexAction():Response
+    public function indexAction(Request $request, LoggerInterface $logger, TranslatorInterface $translator):Response
     {
-        return $this->render('translation/index.html.twig', []);
+        $logger->debug("lorem debug");
+        $logger->alert("alert lorem");
+        $logger->info("info lorem lorem");
+        $logger->notice("notice lorem notice");
+
+        /*
+         * accéder à la locale : à partir de la requête
+         * accéder au service de traduction : TranslatorInterface
+         */
+
+        dump($request->getLocale(), $translator->trans('content.replacement', ['%name%' => 'AliMero']));
+
+//        die;
+
+        return $this->render('translation/index.html.twig', [
+            'now' => new \DateTime()
+        ]);
     }
 
 }
